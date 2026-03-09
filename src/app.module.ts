a0aa22eb2c13c30d860ed1f8c0427a939de50171
OscarGreen27 <andrii.voitenko97@gmail.com>
2026-03-09 22:48:20 +0200

    add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get('DB_USER', 'postgres'),
+        password: configService.get('DB_PASS', 'strong_pass'),
+        database: configService.get('DB_NAME', 'public'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
